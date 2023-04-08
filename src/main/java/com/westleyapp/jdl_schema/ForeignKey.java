bb04ee6902a1ace7190e5e75dfdd0e6dfad46a51
rick <RickPoleshuck@gmail.com>
2023-04-08 15:33:49 -0400

    more

--- src/main/java/com/westleyapp/jdl_schema/ForeignKey.java
+++ src/main/java/com/westleyapp/jdl_schema/ForeignKey.java
@@ -1,27 +1,36 @@
 package com.westleyapp.jdl_schema;
 
 public class ForeignKey {
-    private final String tableName;
-    private final String columnName;
+    private final String foreignTableName;
+    private final String foreignColumnName;
 
-    public ForeignKey(String tableName, String columnName) {
-        this.tableName = tableName;
-        this.columnName = columnName;
+    private final String localColumnName;
+
+    public ForeignKey(final String localColumnName, final String foreignTableName, final String foreignColumnName) {
+        this.localColumnName = localColumnName;
+        this.foreignTableName = foreignTableName;
+        this.foreignColumnName = foreignColumnName;
     }
 
-    public String getTableName() {
-        return tableName;
+    public String getLocalColumnName() {
+        return localColumnName;
     }
 
-    public String getColumnName() {
-        return columnName;
+    public String getForeignTableName() {
+        return foreignTableName;
+    }
+
+    public String getForeignColumnName() {
+        return foreignColumnName;
     }
 
     @Override
     public String toString() {
-        String sb = "ForeignKey{" + "tableName='" + tableName + '\'' +
-                ", columnName='" + columnName + '\'' +
-                '}';
-        return sb;
+        final StringBuilder sb = new StringBuilder("ForeignKey{");
+        sb.append("foreignTableName='").append(foreignTableName).append('\'');
+        sb.append(", foreignColumnName='").append(foreignColumnName).append('\'');
+        sb.append(", localColumnName='").append(localColumnName).append('\'');
+        sb.append('}');
+        return sb.toString();
     }
 }
